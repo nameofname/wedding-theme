97b58f8cb52433fa7d83c6cc393054718557a7cd
nameofname <nameofemail@gmail.com>
2013-01-16 23:52:35 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action( 'init', 'remove_crap' );
+    function remove_crap() {
+
+    //remove_custom_image_header();
+    remove_custom_background();
+    //remove_theme_support('post-formats');
+}
+?>
